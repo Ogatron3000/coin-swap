0901697aa729da6f7beed3bcb2d8e59959580581
Ivan Mijanovic <ivanmijanovic18@gmail.com>
2022-03-12 17:47:56 +0100

    refactored exchange and chart into 2 main windows

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import styles from './App.module.css';
-import Exchange from "./components/Exchange/Exchange";
 import CoinsProvider from "./context/CoinsProvider";
 import {QueryClient, QueryClientProvider} from "react-query";
+import ChartWindow from "./components/ChartWindow/ChartWindow";
+import ExchangeWindow from "./components/ExchangeWindow/ExchangeWindow";
+import {useState} from "react";
 
 const queryClient = new QueryClient()
 
 function App() {
+    const [isChartWindowOpen, setIsChartWindowOpen] = useState(false)
+
+    function toggleChartWindow() {
+        setIsChartWindowOpen(!isChartWindowOpen)
+    }
+
     return (
         <div className={styles.wrapper}>
             <h1 className={styles.h1}>CoinSwap</h1>
             <QueryClientProvider client={queryClient}>
                 <CoinsProvider>
-                    <div className={styles.exchange}>
-                        <Exchange/>
+                    <div className={styles.main}>
+                        <ChartWindow isChartWindowOpen={isChartWindowOpen} toggleChartWindow={toggleChartWindow} />
+                        <ExchangeWindow isChartWindowOpen={isChartWindowOpen} toggleChartWindow={toggleChartWindow} />
                     </div>
                 </CoinsProvider>
             </QueryClientProvider>
--- src/components/ChartWindow/ChartWindow.js
+++ src/components/ChartWindow/ChartWindow.js
@@ -6,7 +6,7 @@ import {useQuery} from "react-query";
 import Chart from "../Chart/Chart";
 import ChartMainInfo from "../ChartMainInfo/ChartMainInfo";
 
-export default function ChartWindow({ isOpen, onClose }) {
+export default function ChartWindow({ isChartWindowOpen, toggleChartWindow }) {
     const [days, setDays] = useState(7)
     const [price, setPrice] = useState(0)
     const [time, setTime] = useState('')
@@ -18,9 +18,9 @@ export default function ChartWindow({ isOpen, onClose }) {
 
     let interval = days > 7 ? 'daily' : 'hourly'
     const {isLoading, data: ratiosArr} = useQuery(
-        ['getRatios', baseCoin.id, targetCoin.id, days, interval],
-        () => getRatios(baseCoin.id, targetCoin.id, days, interval),
-        {refetchOnWindowFocus: false}
+        ['getRatios', baseCoin?.id, targetCoin?.id, days, interval],
+        () => getRatios(baseCoin?.id, targetCoin?.id, days, interval),
+        {refetchOnWindowFocus: false, enabled: !!(baseCoin && targetCoin)}
     )
 
     useEffect(() => {
@@ -47,7 +47,7 @@ export default function ChartWindow({ isOpen, onClose }) {
         }
     }, [ratiosArr])
 
-    if (!isOpen) return null
+    if (!isChartWindowOpen) return null
 
     function swap() {
         setSwapped(!swapped)
@@ -86,7 +86,7 @@ export default function ChartWindow({ isOpen, onClose }) {
                         </svg>
                     </button>
                 </div>
-                <button onClick={onClose}>
+                <button onClick={toggleChartWindow}>
                     <svg viewBox="0 0 24 24" color="primary" width="20px" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
                         <path d="M18.3 5.70997C17.91 5.31997 17.28 5.31997 16.89 5.70997L12 10.59L7.10997 5.69997C6.71997 5.30997 6.08997 5.30997 5.69997 5.69997C5.30997 6.08997 5.30997 6.71997 5.69997 7.10997L10.59 12L5.69997 16.89C5.30997 17.28 5.30997 17.91 5.69997 18.3C6.08997 18.69 6.71997 18.69 7.10997 18.3L12 13.41L16.89 18.3C17.28 18.69 17.91 18.69 18.3 18.3C18.69 17.91 18.69 17.28 18.3 16.89L13.41 12L18.3 7.10997C18.68 6.72997 18.68 6.08997 18.3 5.70997Z"/>
                     </svg>
